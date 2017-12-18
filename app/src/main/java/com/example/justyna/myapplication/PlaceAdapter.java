78842e7e79240b0550ad59a6770fd4af112ddfba
unknown <student@IBLAB19N-01.ib.polsl.pl>
2017-12-18 17:05:16 +0100

    alles gut

--- app/src/main/java/com/example/justyna/myapplication/PlaceAdapter.java
+++ app/src/main/java/com/example/justyna/myapplication/PlaceAdapter.java
@@ -1,9 +1,42 @@
 package com.example.justyna.myapplication;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
 /**
  * Created by student on 2017-12-18.
  */
 
-public class PlaceAdapter {
-    
-}
+public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.ViewHolder> {
+
+
+    @Override
+    public PlaceAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(PlaceAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+    }}
+
+
+
+
+
